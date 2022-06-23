c145c73016d958ac74c19cd8271032681ff6c8ee
s2100628-Alex <alex.degerman.dev@gmail.com>
2022-06-23 06:25:03 +0300

    add product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FilterPipe } from './filter.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe,
+    ProductListComponent,
+    ProductDetailsComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
+    RouterModule.forRoot([{path: "", component: ProductListComponent},
+  {path: "product/:id", component: ProductDetailsComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+  
+  getProducts() {
+    return this.http.get("/assets/phones/phones.json");
+  }
+  getProduct(id: any) {
+    return this.http.get('assets/phones/' + id + '.json')
+  }
+}
